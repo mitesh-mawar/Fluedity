1a834e8e026cf743d3864466134ae4c11e0c82a9
Maxup Technology <miteshmawar30@gmail.com>
2024-10-15 01:06:47 +0530

    Metadata fix, M1

--- data/quampi/metadata.ts
+++ data/quampi/metadata.ts
@@ -1,14 +1,14 @@
 export const PROJECT_USER_AUTH_DESCRIPTION = "I am Mitesh Mawar, I love to create."
 
-export const PROJECT_SUPPORT_EMAIL = "miteshmawar30@gmail.com"
+export const PROJECT_SUPPORT_EMAIL = "quampi.mitesh@gmail.com"
 export const PROJECT_CREATOR_X_HANDLE = "https://www.x.com/miteshmawar"
 
-export const PROJECT_NAME = "Bloakk";
-export const PROJECT_DESCRIPTION = "Showcase your journey interactively with Bloakk - featuring money grids, projects, and sponsorship opportunities.";
+export const PROJECT_NAME = "Quampi";
+export const PROJECT_DESCRIPTION = "Real time analytics with easy integration and advance data studying method. Quampi lets your make good decision with your precious data.";
 export const PROJECT_AUTHOR = "Mitesh Mawar";
-export const PROJECT_KEYWORDS = ["personal branding", "interactive portfolio", "money grid", "sponsorship", "career journey", "project showcase"];
+export const PROJECT_KEYWORDS = ["analytics",];
 
-export const PROJECT_URL = 'https://bloakk.vercel.app'
+export const PROJECT_URL = 'https://quampi.vercel.app'
 
 export const WAITING_LIST =
     process.env.NODE_ENV == "development" ? false : false
